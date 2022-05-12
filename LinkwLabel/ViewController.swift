6c9f7f6aa8f6b8f88d9088c2cb5115f66db75fd8
Franco Rod <francorodios@gmail.com>
2022-05-12 10:05:56 -0400

    coding label link

--- LinkwLabel/ViewController.swift
+++ LinkwLabel/ViewController.swift
@@ -7,13 +7,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate {
+    @IBOutlet var textView: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let attributedString = NSMutableAttributedString(string: "What to lear iOS?")
+        attributedString.addAttribute(.link, value: "https://www.apple.com", range: NSRange(location: 19, length: 55))
+        
+        textView.attributedText = attributedString
     }
-
-
+    func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
+        UIApplication.shared.open(URL)
+        return false
+    }
+    
 }
+
 
